1e44d37acbefbba50720e7a204c173a554c5af9d
wickbushong <wickbushong@gmail.com>
2020-07-09 11:24:39 -0400

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, info|
+    if person == "Freddy Mercury"
+      info.each do |key, value|
+        if key == :favorite_ice_cream_flavors
+          value.delete_if {|value| value == "strawberry"}
+        end
+      end
+    end
+  end
 end
