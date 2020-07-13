d354c0fe72420ad7c4766fc439dc5464d7477b96
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-13 21:54:26 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,24 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+=begin
+  contacts.each do |key, value|
+    if key == :favorite_ice_cream_flavors
+      if value.include?("strawberry")
+        value.shift
+      end
+    end
+  end
+=end
+  contacts.each do |person, value_hash|
+    if person == "Freddy Mercury"
+      value_hash.each do |key, value|
+        if key == :favorite_ice_cream_flavors
+          value.delete_if do |ice_cream|
+            ice_cream == "strawberry"
+          end
+        end
+      end
+    end
+  end
 end
